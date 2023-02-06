973e097da4a3322a9d23429a3cd44597c69cf599
AnimBadger <anim.ansah.stephen@gmail.com>
2023-02-06 23:30:55 +0000

    shallow fix

--- models/base_model.py
+++ models/base_model.py
@@ -20,17 +20,6 @@ class BaseModel:
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
-        if len(kwargs) != 0:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    self.__dict__[key] = datetime.strptime(value, date_format)
-                else:
-                    self.__dict__[key] = value
-
-    def __str__(self):
-        '''Return string representation of object'''
-        clsname = self.__class__.__name__
-        return "[{}] ({}) {}".format(clsname, self.id, self.__dict__)
     
     def save(self):
         '''Update datetime'''
@@ -42,3 +31,8 @@ class BaseModel:
         new_dict['updated_at'] = self.updated_at.isoformat()
         new_dict['__class__'] = self.__class__.__name__
         return new_dict
+
+    def __str__(self):
+        '''Return string representation of object'''
+        clsname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clsname, self.id, self.__dict__)
