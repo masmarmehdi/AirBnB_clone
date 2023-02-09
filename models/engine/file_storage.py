2adf2cb657e303b10ecda6dcf4a6bb0bc971e204
AnimBadger <anim.ansah.stephen@gmail.com>
2023-02-09 15:23:24 +0000

    made imports to FileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,11 @@ and Deserialize Json file to instances
 '''
 from models.base_model import BaseModel
 from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 import json
 
 class FileStorage:
